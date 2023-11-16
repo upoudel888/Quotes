4d4524f638878dd1879b6d0c92b5238e2db40e56
Ujjwal Paudel <upoudel888@gmail.com>
2023-11-16 15:17:35 +0545

    Stylings changed

--- README.md
+++ README.md
@@ -2,14 +2,11 @@
 
 ## Anonymously Share Wisdom
 
-Quotes is a React-powered platform enabling users to anonymously post and discover impactful quotes from famous personalities. Embrace motivation through the power of shared wisdom.
+Quotes is a React-powered platform (Just Frontend)enabling users to anonymously post and discover impactful quotes from famous personalities. Embrace motivation through the power of shared wisdom.
 
 ### Features:
 
 - **Anonymous Posting:** Share inspiring quotes without revealing your identity.
-  
-- **Curated Collection:** Explore a diverse range of quotes from renowned figures.
-
 - **Simple Interface:** User-friendly design for effortless contribution and exploration.
 
 ### Technologies:
--- src/App.js
+++ src/App.js
@@ -7,14 +7,20 @@ import {BrowserRouter as Router,Routes,Route,} from "react-router-dom";
 import { QuoteProvider } from './QuoteContext';
 // assets
 import logo from './assets/logo.svg';
+import { useNavigate } from 'react-router-dom';
 // import { FaInstagram,FaLinkedin,FaFacebook,FaGithub} from "react-icons/fa";
 
 
 const Navbar = ()=>{
+    // to route back button to home page
+    const navigate = useNavigate();
+    const routeToHome = ()=>{
+        navigate("/");    
+    }
   return (
     <nav>
         <div className="logo">
-          <img className="scale-in-hor-left " src={logo} alt="logo" />
+          <img className="scale-in-hor-left " src={logo} alt="logo"  onClick = {routeToHome} />
           <div className="quotes rotate-in-2-ccw">&#10078;</div>
         </div>
       </nav>
@@ -32,18 +38,18 @@ function App() {
 
   return (
     <>
-      <Navbar></Navbar>
 
       <Router>
+        <Navbar></Navbar>
         <QuoteProvider>
         <Routes>
             <Route path = "/" exact element={<PostsPage/>}/>
             <Route path = "/comments/:id"  exact element={<CommentPage/> }/>
         </Routes>
         </QuoteProvider>
+        <Footer></Footer>
       </Router>
 
-      <Footer></Footer>
 
     </>
     
